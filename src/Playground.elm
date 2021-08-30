0b7dbd94806ba30ede90923032abe24ddb784fbc
Vest <Vest@users.noreply.github.com>
2021-08-30 11:16:20 +0200

    Added regex and sorting

--- src/Playground.elm
+++ src/Playground.elm
@@ -116,7 +116,21 @@ hashtag dayInNumber =
             "#Whatever"
 
 
+descending a b =
+    case compare a b of
+        LT ->
+            GT
+
+        GT ->
+            LT
+
+        EQ ->
+            EQ
+
+
 main : Html.Html msg
 main =
-    hashtag 5
+    [ 316, 320, 312, 370, 337, 318, 314 ]
+        |> List.sortWith descending
+        |> Debug.toString
         |> Html.text
