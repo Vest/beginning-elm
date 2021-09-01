323aa24e7a7de330bb05049772988813a71dbe7f
Vest <Vest@users.noreply.github.com>
2021-09-01 17:08:43 +0200

    Added more tests (with copy-paste). No, I will not type this, don't ask me.
    Signed-off-by: Vest <Vest@users.noreply.github.com>

--- tests/RippleCarryAdderTests.elm
+++ tests/RippleCarryAdderTests.elm
@@ -1,4 +1,10 @@
-module RippleCarryAdderTests exposing (andGateTests, inverterTests, orGateTests)
+module RippleCarryAdderTests exposing
+    ( andGateTests
+    , fullAdderTests
+    , halfAdderTests
+    , inverterTests
+    , orGateTests
+    )
 
 import Expect exposing (Expectation)
 import Fuzz exposing (..)
@@ -59,4 +65,62 @@ orGateTests =
             \_ ->
                 orGate 1 1
                     |> Expect.equal 1
+        ]
+
+
+halfAdderTests =
+    describe "Half adder"
+        [ test "sum and carry-out are 0 when both inputs are 0" <|
+            \_ ->
+                halfAdder 0 0
+                    |> Expect.equal { carry = 0, sum = 0 }
+        , test "sum is 1 and carry-out is 0 when the 1st input is 0 and the 2nd input is 1" <|
+            \_ ->
+                halfAdder 0 1
+                    |> Expect.equal { carry = 0, sum = 1 }
+        , test "sum is 1 and carry-out is 0 when the 1st input is 1 and the 2nd input is 0" <|
+            \_ ->
+                halfAdder 1 0
+                    |> Expect.equal { carry = 0, sum = 1 }
+        , test "sum is 0 and carry-out is 1 when both inputs are 1" <|
+            \_ ->
+                halfAdder 1 1
+                    |> Expect.equal { carry = 1, sum = 0 }
+        ]
+
+
+fullAdderTests =
+    describe "Full adder"
+        [ test "sum and carry-out are 0 when both inputs and carry-in are 0" <|
+            \_ ->
+                fullAdder 0 0 0
+                    |> Expect.equal { carry = 0, sum = 0 }
+        , test "sum is 1 and carry-out is 0 when both inputs are 0, but carry-in is 1" <|
+            \_ ->
+                fullAdder 0 0 1
+                    |> Expect.equal { carry = 0, sum = 1 }
+        , test "sum is 1 and carry-out is 0 when the 1st input is 0, the 2nd input is 1, and carry-in is 0" <|
+            \_ ->
+                fullAdder 0 1 0
+                    |> Expect.equal { carry = 0, sum = 1 }
+        , test "sum is 0 and carry-out is 1 when the 1st input is 0, the 2nd input is 1, and the carry-in is 1" <|
+            \_ ->
+                fullAdder 0 1 1
+                    |> Expect.equal { carry = 1, sum = 0 }
+        , test "sum is 1 and carry-out is 0 when the 1st input is 1, the 2nd input is 0, and the carry-in is 0" <|
+            \_ ->
+                fullAdder 1 0 0
+                    |> Expect.equal { carry = 0, sum = 1 }
+        , test "sum is 0 and carry-out is 1 when the 1st input is 1, the 2nd input is 0, and the carry-in is 1" <|
+            \_ ->
+                fullAdder 1 0 1
+                    |> Expect.equal { carry = 1, sum = 0 }
+        , test "sum is 0 and carry-out is 1 when the 1st input is 1, the 2nd input is 1, and the carry-in is 0" <|
+            \_ ->
+                fullAdder 1 1 0
+                    |> Expect.equal { carry = 1, sum = 0 }
+        , test "sum is 1 and carry-out is 1 when the 1st input is 1, the 2nd input is 1, and the carry-in is 1" <|
+            \_ ->
+                fullAdder 1 1 1
+                    |> Expect.equal { carry = 1, sum = 1 }
         ]
