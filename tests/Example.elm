925a7fe52970208249379dcb3465c9feac3886c6
Vest <Vest@users.noreply.github.com>
2021-09-01 16:53:03 +0200

    Added even more tests.
    Signed-off-by: Vest <Vest@users.noreply.github.com>

--- tests/Example.elm
+++ tests/Example.elm
@@ -1,4 +1,4 @@
-module Example exposing (additionTests, guardianNames)
+module Example exposing (additionTests, comparisonTests, guardianNames)
 
 import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, int, list, string)
@@ -25,4 +25,27 @@ additionTests =
             \_ -> (2 + 2) |> Expect.equal 4
         , test "three plus four equals seven" <|
             \_ -> (3 + 4) |> Expect.equal 7
+        ]
+
+
+comparisonTests =
+    describe "Comparison"
+        [ test "2 is not equal to 3" <|
+            \_ -> 2 |> Expect.notEqual 3
+        , test "4 is less than 5" <|
+            \_ -> 4 |> Expect.lessThan 5
+        , test "6 is less than or equal to 7" <|
+            \_ -> 6 |> Expect.atMost 7
+        , test "9 is greater than 8" <|
+            \_ -> 9 |> Expect.greaterThan 8
+        , test "11 is greater than or equal to 10" <|
+            \_ -> 11 |> Expect.atLeast 10
+        , test "a list with zero elements is empty" <|
+            \_ ->
+                List.isEmpty []
+                    |> Expect.true "expected the list to be empty"
+        , test "a list with some elements is not empty" <|
+            \_ ->
+                List.isEmpty [ "Jyn", "Cassian", "K-2SO" ]
+                    |> Expect.false "expected the list not to be empty"
         ]
