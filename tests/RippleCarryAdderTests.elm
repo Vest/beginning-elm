5e17dec49635715b2dc596d3b7e177e28d536522
Vest <Vest@users.noreply.github.com>
2021-09-01 18:01:16 +0200

    Completed the "test" exercise.
    Signed-off-by: Vest <Vest@users.noreply.github.com>

--- tests/RippleCarryAdderTests.elm
+++ tests/RippleCarryAdderTests.elm
@@ -4,6 +4,7 @@ module RippleCarryAdderTests exposing
     , halfAdderTests
     , inverterTests
     , orGateTests
+    , rippleCarryAdderTests
     )
 
 import Expect exposing (Expectation)
@@ -91,36 +92,95 @@ halfAdderTests =
 
 fullAdderTests =
     describe "Full adder"
-        [ test "sum and carry-out are 0 when both inputs and carry-in are 0" <|
-            \_ ->
-                fullAdder 0 0 0
-                    |> Expect.equal { carry = 0, sum = 0 }
-        , test "sum is 1 and carry-out is 0 when both inputs are 0, but carry-in is 1" <|
-            \_ ->
-                fullAdder 0 0 1
-                    |> Expect.equal { carry = 0, sum = 1 }
-        , test "sum is 1 and carry-out is 0 when the 1st input is 0, the 2nd input is 1, and carry-in is 0" <|
-            \_ ->
-                fullAdder 0 1 0
-                    |> Expect.equal { carry = 0, sum = 1 }
-        , test "sum is 0 and carry-out is 1 when the 1st input is 0, the 2nd input is 1, and the carry-in is 1" <|
-            \_ ->
-                fullAdder 0 1 1
-                    |> Expect.equal { carry = 1, sum = 0 }
-        , test "sum is 1 and carry-out is 0 when the 1st input is 1, the 2nd input is 0, and the carry-in is 0" <|
-            \_ ->
-                fullAdder 1 0 0
-                    |> Expect.equal { carry = 0, sum = 1 }
-        , test "sum is 0 and carry-out is 1 when the 1st input is 1, the 2nd input is 0, and the carry-in is 1" <|
-            \_ ->
-                fullAdder 1 0 1
-                    |> Expect.equal { carry = 1, sum = 0 }
-        , test "sum is 0 and carry-out is 1 when the 1st input is 1, the 2nd input is 1, and the carry-in is 0" <|
-            \_ ->
-                fullAdder 1 1 0
-                    |> Expect.equal { carry = 1, sum = 0 }
-        , test "sum is 1 and carry-out is 1 when the 1st input is 1, the 2nd input is 1, and the carry-in is 1" <|
-            \_ ->
-                fullAdder 1 1 1
-                    |> Expect.equal { carry = 1, sum = 1 }
+        [ describe "when both inputs are 0"
+            [ describe "and carry-in is 0"
+                [ test "both sum and carry-out are 0" <|
+                    \_ ->
+                        fullAdder 0 0 0
+                            |> Expect.equal { carry = 0, sum = 0 }
+                ]
+            , describe "but carry-out is 1"
+                [ test "sum is 1 and carry-out is 0" <|
+                    \_ ->
+                        fullAdder 0 0 1
+                            |> Expect.equal { carry = 0, sum = 1 }
+                ]
+            ]
+        , describe "when the 1st input is 0"
+            [ describe "and the 2nd input is 1"
+                [ describe "and carry-in is 0"
+                    [ test "sum is 1 and carry-out is 0" <|
+                        \_ ->
+                            fullAdder 0 1 0
+                                |> Expect.equal { carry = 0, sum = 1 }
+                    ]
+                , describe "and carry-in is 1"
+                    [ test "sum is 0 and carry-out is 1" <|
+                        \_ ->
+                            fullAdder 0 1 1
+                                |> Expect.equal { carry = 1, sum = 0 }
+                    ]
+                ]
+            ]
+        , describe "when the 1st input is 1"
+            [ describe "and the 2nd input is 0"
+                [ describe "and carry-in is 0"
+                    [ test "sum is 1 and carry-out is 0" <|
+                        \_ ->
+                            fullAdder 1 0 0
+                                |> Expect.equal { carry = 0, sum = 1 }
+                    ]
+                , describe "and carry-in is 1"
+                    [ test "sum is 0 and carry-out is 1" <|
+                        \_ ->
+                            fullAdder 1 0 1
+                                |> Expect.equal { carry = 1, sum = 0 }
+                    ]
+                ]
+            , describe "and the 2nd input is 1"
+                [ describe "and carry-in is 0"
+                    [ test "sum is 0 and carry-out is 1" <|
+                        \_ ->
+                            fullAdder 1 1 0
+                                |> Expect.equal { carry = 1, sum = 0 }
+                    ]
+                , describe "and carry-in is 1"
+                    [ test "sum is 1 and carry-out is 1" <|
+                        \_ ->
+                            fullAdder 1 1 1
+                                |> Expect.equal { carry = 1, sum = 1 }
+                    ]
+                ]
+            ]
+        ]
+
+
+rippleCarryAdderTests =
+    describe "4-bit ripple carry adder"
+        [ describe "given two binary numbers and a carry-in digit"
+            [ test "returns the sum of those numbers and a carry-out digit" <|
+                \_ ->
+                    rippleCarryAdder 1001 1101 1
+                        |> Expect.equal 10111
+            ]
+        , describe "when the 1st input is 1111, and the 2nd input is 1111"
+            [ test "and carry-in is 0, the output is 11110" <|
+                \_ ->
+                    rippleCarryAdder 1111 1111 0
+                        |> Expect.equal 11110
+            , test "and carry-in is 1, the output is 11111" <|
+                \_ ->
+                    rippleCarryAdder 1111 1111 1
+                        |> Expect.equal 11111
+            ]
+        , describe "when the 1st input is 0000, and the 2nd input is 0000"
+            [ test "and carry-in is 0, the output is 0000" <|
+                \_ ->
+                    rippleCarryAdder 0 0 0
+                        |> Expect.equal 0
+            , test "and carry-in is 1, the output is 0001" <|
+                \_ ->
+                    rippleCarryAdder 0 0 1
+                        |> Expect.equal 1
+            ]
         ]
