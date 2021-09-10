1f323bc379e678633a669988db33544687f091af
Vest <Vest@users.noreply.github.com>
2021-09-10 16:57:12 +0200

    Added fuzz2
    Signed-off-by: Vest <Vest@users.noreply.github.com>

--- tests/FuzzTests.elm
+++ tests/FuzzTests.elm
@@ -12,9 +12,27 @@ addOneTests =
         [ fuzz int "adds 1 to any integer" <|
             \num ->
                 addOne num |> Expect.equal (num + 1)
+        , test "when 1 is added to 2, the result is 3" <|
+            \_ ->
+                addOne 2 |> Expect.equal 3
+        ]
+
+
+addTests : Test
+addTests =
+    describe "add"
+        [ fuzz2 int int "adds tw given integers" <|
+            \num1 num2 ->
+                add num1 num2
+                    |> Expect.equal (num1 + num2)
         ]
 
 
 addOne : Int -> Int
 addOne x =
     1 + x
+
+
+add : Int -> Int -> Int
+add x y =
+    x + y
