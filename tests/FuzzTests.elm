fbf7903332ef1ce7d62a64749d3b746e999a82ad
Vest <Vest@users.noreply.github.com>
2021-10-05 16:02:52 +0200

    Added more fuzzy tests
    Signed-off-by: Vest <Vest@users.noreply.github.com>

--- tests/FuzzTests.elm
+++ tests/FuzzTests.elm
@@ -1,8 +1,9 @@
-module FuzzTests exposing (addOneTests)
+module FuzzTests exposing (addOneTests, addTests)
 
 import Expect exposing (Expectation)
 import Fuzz exposing (..)
 import Http exposing (Expect)
+import Random exposing (maxInt, minInt)
 import Test exposing (..)
 
 
@@ -15,13 +16,22 @@ addOneTests =
         , test "when 1 is added to 2, the result is 3" <|
             \_ ->
                 addOne 2 |> Expect.equal 3
+        , fuzzWith { runs = 200 } int "adds 1 to the given integer" <|
+            \num ->
+                addOne num |> Expect.equal (num + 1)
+        , fuzz (intRange -100 100) "adds 1 to the given integer in a range" <|
+            \num ->
+                addOne num |> Expect.equal (num + 1)
+        , fuzz (intRange minInt maxInt) "adds 1 to all integers" <|
+            \num ->
+                addOne num |> Expect.equal (num + 1)
         ]
 
 
 addTests : Test
 addTests =
     describe "add"
-        [ fuzz2 int int "adds tw given integers" <|
+        [ fuzz2 int int "adds two given integers" <|
             \num1 num2 ->
                 add num1 num2
                     |> Expect.equal (num1 + num2)
